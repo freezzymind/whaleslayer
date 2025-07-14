ccec737b09de6a295cb4ed70750986b260ae172e
freezzymind <tsentnerphilipp6@gmail.com>
2025-07-14 14:08:25 +1000

    Подготовил БД к первому деплою.

--- database/entrypoint.sh
+++ database/entrypoint.sh
@@ -6,5 +6,8 @@ envsubst < ./init.template.sql > ./init.generated.sql
 sudo chown 999:999 ./init.generated.sql
 sudo chmod 644 ./init.generated.sql
 
+echo "[DEBUG] Переменные окружения:"
+env | grep POSTGRES
+
 # Запуск docker-compose с уже подготовленным init.sql
 docker compose up -d
