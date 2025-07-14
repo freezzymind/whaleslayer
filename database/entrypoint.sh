80ee3e8d8433a1db308233aea561ba659494ac3b
freezzymind <tsentnerphilipp6@gmail.com>
2025-07-14 17:07:23 +1000

    Подготовил БД к первому деплою.

--- database/entrypoint.sh
+++ database/entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
+set -a
+source ../.env
+set +a
+
 # Подстановка переменных в шаблон init.template.sql → init.generated.sql
 envsubst < ./init.template.sql > ./init.generated.sql
 
 sudo chown 999:999 ./init.generated.sql
 sudo chmod 644 ./init.generated.sql
-
-set -a
-source ../.env
-set +a
 
 # Запуск docker-compose с уже подготовленным init.sql
 docker compose up -d
